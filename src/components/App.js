6f954b5e9ee161f9e4698b24e423903d16669770
shananasim16 <shanashabnam02@gmail.com>
2024-06-28 13:03:26 +0530

    new changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,18 @@
+import React from "react";
 import './../styles/App.css';
 
-import React ,{useState, useEffect} from 'react';
-
 const App = () => {
-  const [weatherInput, setWeatherInput] =useState({ temperature: 25, conditions: "Sunny" });
-  const [textColor, setTextColor] = useState("blue");
-  useEffect(() => {
-    // Set text color based on temperature threshold
-    if (weatherInput.temperature > 20) {
-      setTextColor("red");
-    } else {
-      setTextColor("blue");
-    }
-  }, [weatherInput.temperature]);
- 
+  const weather = {
+    temperature: 25, 
+    conditions: "Sunny"
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
-       <div style ={{color:textColor}}>
-        <p>Temperature: {weatherInput.temperature}</p>
-        <p>Conditions: {weatherInput.conditions}</p>
-       </div>
+        <p style={weather.temperature> 20?{backgroundColor:'red'}:{backgroundColor:'blue'}}>Temperature: {weather.temperature}</p>
+        <span > Condition: {weather.conditions}</span>
+
     </div>
   )
 }
-
 
 export default App
